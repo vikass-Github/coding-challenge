8c8d6e90e1855acb70bd00a592c65adade595312
TheGuyfromEarth <rishabhgupta01994@gmail.com>
2023-08-22 19:59:16 +0530

    Initial commit

---
+++ src/test/java/com/coding/Java/Coding/Challenge2/JavaCodingChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.Java.Coding.Challenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingChallenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
